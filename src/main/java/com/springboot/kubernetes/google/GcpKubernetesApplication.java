7f0f921c353f99e5b23ebb97cf87c8194351a126
inho.kim <inho.kim@softwareinlife.com>
2022-03-21 20:11:20 +0900

    Main Application Name Change

--- src/main/java/com/springboot/kubernetes/google/GcpKubernetesApplication.java
+++ src/main/java/com/springboot/kubernetes/google/GcpKubernetesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GoogleApplication {
+public class GcpKubernetesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GoogleApplication.class, args);
+		SpringApplication.run(GcpKubernetesApplication.class, args);
 	}
 
 }
