3b30ab40e4556d2e37c00ce4f421f1b787c06fad
inho.kim <inho.kim@softwareinlife.com>
2022-03-21 20:08:06 +0900

    Initial commit

---
+++ src/main/java/com/springboot/kubernetes/google/service/kubernetes/HealthCheckRest.java
@@ -0,0 +1,21 @@
+package com.springboot.kubernetes.google.service.kubernetes;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 쿠버네티스 인그레스 상태 체크 확인 Rest
+ *
+ * @Date 2022/03/21
+ * @Author inho.kim (inho.kim@softwareinlife.com)
+ */
+@RestController
+@Slf4j
+public class HealthCheckRest {
+
+    @RequestMapping("/health")
+    public String healthCheckRest() {
+        return "200 Ok.";
+    }
+}
